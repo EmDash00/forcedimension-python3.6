88ec62c63006130b47389a361623d9b4393b60eb
Ember Chow <emberchow.businiess@iu.edu>
2023-10-04 23:36:00 -0700

    Convert pyproject.toml to setup.py for 3.6

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
 from setuptools import setup
 
 if __name__ == '__main__':
-    setup()
+    setup(
+        name='forcedimension3.6',
+        version='0.1.0',
+        python_requires=">=3.6 <3.7",
+        install_requires=[
+            "setuptools>=42",
+            "wheel",
+            "typing_extensions >= 4.1.1, <5",
+            "pydantic>=1.9.2, <2",
+            "numpy>=1.16.4, <2"
+        ]
+    )
