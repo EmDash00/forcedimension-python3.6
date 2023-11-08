75d94193c8b8f3217f106481dfbb766ee472630e
Ember Chow <emberchow.businiess@iu.edu>
2023-11-08 01:47:13 -0800

    retry setup.py

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from setuptools import find_packages, setup
 
 if __name__ == '__main__':
     setup(
-        # name='forcedimension',
-        # packages=find_packages(),
-        # version='0.2.0b2',
-        # python_requires=">=3.6, <3.7",
-        # install_requires=[
-            # "setuptools>=42",
-            # "wheel",
-            # "typing_extensions >= 4.1.1, <5",
-            # "PyYAML>=6.0.1, <7",
-            # "forcedimension-core"
-        # ],
-        # extras_require = {
-            # 'forcedimension-core': ['numpy']
-        # },
-        # dependency_links=[
-            # "git+https://github.com/EmDash00/forcedimension-core-python3.6.git"
-            # "@v1.0.0#egg=forcedimension-core"
-        # ]
+        name='forcedimension',
+        packages=find_packages(),
+        version='0.2.0b2',
+        python_requires=">=3.6, <3.7",
+        install_requires=[
+            "setuptools>=42",
+            "wheel",
+            "typing_extensions >= 4.1.1, <5",
+            "PyYAML>=6.0.1, <7",
+            "forcedimension-core"
+        ],
+        extras_require = {
+            'forcedimension-core': ['numpy']
+        },
+        dependency_links=[
+            "git+https://github.com/EmDash00/forcedimension-core-python3.6.git"
+            "@v1.0.0#egg=forcedimension-core"
+        ]
     )
