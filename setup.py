be735e5223b82dd1a89f9f51948cb4dc67b3755a
Ember Chow <emberchow.businiess@iu.edu>
2023-11-08 01:34:38 -0800

    Try to use pyproject.toml

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import find_packages, setup
 
 if __name__ == '__main__':
     setup(
-        name='forcedimension',
-        packages=find_packages(),
-        version='0.2.0b2',
-        python_requires=">=3.6, <3.7",
-        install_requires=[
-            "setuptools>=42",
-            "wheel",
-            "typing_extensions >= 4.1.1, <5",
-            "PyYAML>=6.0.1, <7"
-        ],
-        dependency_links=[
-            "forcedimension-core[numpy] @ "
-            "git+https://github.com/EmDash00/forcedimension-core-python3.6.git"
-            "@v1.0.0"
-        ]
+        # name='forcedimension',
+        # packages=find_packages(),
+        # version='0.2.0b2',
+        # python_requires=">=3.6, <3.7",
+        # install_requires=[
+            # "setuptools>=42",
+            # "wheel",
+            # "typing_extensions >= 4.1.1, <5",
+            # "PyYAML>=6.0.1, <7"
+        # ],
+        # dependency_links=[
+            # "forcedimension-core[numpy] @ "
+            # "git+https://github.com/EmDash00/forcedimension-core-python3.6.git"
+            # "@v1.0.0"
+        # ]
     )
